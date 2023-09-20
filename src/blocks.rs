695907f4686cdd50a675429f0cd4317a4409acc1
Red Penguin <timmyterskov@gmail.com>
2023-09-20 19:38:37 +0100

    re-added I block

--- src/blocks.rs
+++ src/blocks.rs
@@ -1,57 +1,7 @@
-use gemini_engine::elements::view::{utils, ColChar, Colour, Point, Vec2D, ViewElement};
+use gemini_engine::elements::view::{utils, ColChar, Point, Vec2D, ViewElement};
 use rand::seq::SliceRandom;
-
-struct BlockData {
-    rotation_states: Vec<Vec<Vec2D>>,
-    colour: Colour,
-}
-impl BlockData {
-    fn new(rotation_states: Vec<Vec<Vec2D>>, colour: Colour) -> BlockData {
-        BlockData {
-            rotation_states,
-            colour,
-        }
-    }
-}
-impl From<BlockType> for BlockData {
-    fn from(value: BlockType) -> Self {
-        match value {
-            BlockType::O => BlockData::new(
-                vec![vec![
-                    Vec2D::new(0, 0),
-                    Vec2D::new(1, 0),
-                    Vec2D::new(0, 1),
-                    Vec2D::new(1, 1),
-                ]],
-                Colour::rgb(255, 255, 0),
-            ),
-            BlockType::I => BlockData::new(
-                vec![],
-                Colour::rgb(0, 255, 255),
-            ),
-            BlockType::T => BlockData::new(
-                vec![],
-                Colour::rgb(255, 0, 255),
-            ),
-            BlockType::S => BlockData::new(
-                vec![],
-                Colour::rgb(0, 255, 0),
-            ),
-            BlockType::Z => BlockData::new(
-                vec![],
-                Colour::rgb(255, 0, 0),
-            ),
-            BlockType::L => BlockData::new(
-                vec![],
-                Colour::rgb(255, 165, 0),
-            ),
-            BlockType::J => BlockData::new(
-                vec![],
-                Colour::rgb(0, 0, 255),
-            ),
-        }
-    }
-}
+mod block_data;
+use block_data::BlockData;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum BlockType {
---
+++ src/blocks/block_data.rs
@@ -0,0 +1,79 @@
+use gemini_engine::elements::{view::Colour, Vec2D};
+use super::BlockType;
+
+pub(super) struct BlockData {
+    pub rotation_states: Vec<Vec<Vec2D>>,
+    pub colour: Colour,
+}
+impl BlockData {
+    fn new(rotation_states: Vec<Vec<Vec2D>>, colour: Colour) -> BlockData {
+        BlockData {
+            rotation_states,
+            colour,
+        }
+    }
+}
+impl From<BlockType> for BlockData {
+    fn from(value: BlockType) -> Self {
+        match value {
+            BlockType::O => BlockData::new(
+                vec![vec![
+                    Vec2D::new(0, 0),
+                    Vec2D::new(1, 0),
+                    Vec2D::new(0, 1),
+                    Vec2D::new(1, 1),
+                ]],
+                Colour::rgb(255, 255, 0),
+            ),
+            BlockType::I => BlockData::new(
+                vec![
+					vec![
+						Vec2D::new(-1, 0),
+						Vec2D::new(0, 0),
+						Vec2D::new(1, 0),
+						Vec2D::new(2, 0),
+					],
+					vec![
+						Vec2D::new(1, -1),
+						Vec2D::new(1, 0),
+						Vec2D::new(1, 1),
+						Vec2D::new(1, 2),
+					],
+					vec![
+						Vec2D::new(-1, 1),
+						Vec2D::new(0, 1),
+						Vec2D::new(1, 1),
+						Vec2D::new(2, 1),
+					],
+					vec![
+						Vec2D::new(0, -1),
+						Vec2D::new(0, 0),
+						Vec2D::new(0, 1),
+						Vec2D::new(0, 2),
+					],
+                ],
+                Colour::rgb(0, 255, 255),
+            ),
+            BlockType::T => BlockData::new(
+                vec![],
+                Colour::rgb(255, 0, 255),
+            ),
+            BlockType::S => BlockData::new(
+                vec![],
+                Colour::rgb(0, 255, 0),
+            ),
+            BlockType::Z => BlockData::new(
+                vec![],
+                Colour::rgb(255, 0, 0),
+            ),
+            BlockType::L => BlockData::new(
+                vec![],
+                Colour::rgb(255, 165, 0),
+            ),
+            BlockType::J => BlockData::new(
+                vec![],
+                Colour::rgb(0, 0, 255),
+            ),
+        }
+    }
+}
