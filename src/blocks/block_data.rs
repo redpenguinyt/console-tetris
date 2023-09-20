20b1526abc1b5e4b4888df88fce15c7e480f4857
Red Penguin <timmyterskov@gmail.com>
2023-09-20 20:11:26 +0100

    added in the rest of the block rotations

--- src/blocks/block_data.rs
+++ src/blocks/block_data.rs
@@ -1,5 +1,5 @@
-use gemini_engine::elements::{view::Colour, Vec2D};
 use super::BlockType;
+use gemini_engine::elements::{view::Colour, Vec2D};
 
 pub(super) struct BlockData {
     pub rotation_states: Vec<Vec<Vec2D>>,
@@ -27,51 +27,176 @@ impl From<BlockType> for BlockData {
             ),
             BlockType::I => BlockData::new(
                 vec![
-					vec![
-						Vec2D::new(-1, 0),
-						Vec2D::new(0, 0),
-						Vec2D::new(1, 0),
-						Vec2D::new(2, 0),
-					],
-					vec![
-						Vec2D::new(1, -1),
-						Vec2D::new(1, 0),
-						Vec2D::new(1, 1),
-						Vec2D::new(1, 2),
-					],
-					vec![
-						Vec2D::new(-1, 1),
-						Vec2D::new(0, 1),
-						Vec2D::new(1, 1),
-						Vec2D::new(2, 1),
-					],
-					vec![
-						Vec2D::new(0, -1),
-						Vec2D::new(0, 0),
-						Vec2D::new(0, 1),
-						Vec2D::new(0, 2),
-					],
+                    vec![
+                        Vec2D::new(-1, 0),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(1, 0),
+                        Vec2D::new(2, 0),
+                    ],
+                    vec![
+                        Vec2D::new(1, -1),
+                        Vec2D::new(1, 0),
+                        Vec2D::new(1, 1),
+                        Vec2D::new(1, 2),
+                    ],
+                    vec![
+                        Vec2D::new(-1, 1),
+                        Vec2D::new(0, 1),
+                        Vec2D::new(1, 1),
+                        Vec2D::new(2, 1),
+                    ],
+                    vec![
+                        Vec2D::new(0, -1),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(0, 1),
+                        Vec2D::new(0, 2),
+                    ],
                 ],
                 Colour::rgb(0, 255, 255),
             ),
             BlockType::T => BlockData::new(
-                vec![],
+                vec![
+                    vec![
+                        Vec2D::new(0, -1),
+                        Vec2D::new(-1, 0),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(1, 0),
+                    ],
+                    vec![
+                        Vec2D::new(0, -1),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(1, 0),
+                        Vec2D::new(0, 1),
+                    ],
+                    vec![
+                        Vec2D::new(-1, 0),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(1, 0),
+                        Vec2D::new(0, 1),
+                    ],
+                    vec![
+                        Vec2D::new(0, -1),
+                        Vec2D::new(-1, 0),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(0, 1),
+                    ],
+                ],
                 Colour::rgb(255, 0, 255),
             ),
             BlockType::S => BlockData::new(
-                vec![],
+                vec![
+                    vec![
+                        Vec2D::new(-1, 0),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(0, -1),
+                        Vec2D::new(1, -1),
+                    ],
+                    vec![
+                        Vec2D::new(0, -1),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(1, 0),
+                        Vec2D::new(1, 1),
+                    ],
+                    vec![
+                        Vec2D::new(0, 0),
+                        Vec2D::new(1, 0),
+                        Vec2D::new(-1, 1),
+                        Vec2D::new(0, 1),
+                    ],
+                    vec![
+                        Vec2D::new(0, 0),
+                        Vec2D::new(0, 1),
+                        Vec2D::new(-1, -1),
+                        Vec2D::new(-1, 0),
+                    ],
+                ],
                 Colour::rgb(0, 255, 0),
             ),
             BlockType::Z => BlockData::new(
-                vec![],
+                vec![
+                    vec![
+                        Vec2D::new(-1, -1),
+                        Vec2D::new(0, -1),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(1, 0),
+                    ],
+                    vec![
+                        Vec2D::new(0, 0),
+                        Vec2D::new(0, 1),
+                        Vec2D::new(1, -1),
+                        Vec2D::new(1, 0),
+                    ],
+                    vec![
+                        Vec2D::new(-1, 0),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(0, 1),
+                        Vec2D::new(1, 1),
+                    ],
+                    vec![
+                        Vec2D::new(0, -1),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(-1, 0),
+                        Vec2D::new(-1, 1),
+                    ],
+                ],
                 Colour::rgb(255, 0, 0),
             ),
             BlockType::L => BlockData::new(
-                vec![],
+                vec![
+                    vec![
+                        Vec2D::new(-1, 0),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(1, -1),
+                        Vec2D::new(1, 0),
+                    ],
+                    vec![
+                        Vec2D::new(0, -1),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(0, 1),
+                        Vec2D::new(1, 1),
+                    ],
+                    vec![
+                        Vec2D::new(-1, 0),
+                        Vec2D::new(-1, 1),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(1, 0),
+                    ],
+                    vec![
+                        Vec2D::new(-1, -1),
+                        Vec2D::new(0, -1),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(0, 1),
+                    ],
+                ],
                 Colour::rgb(255, 165, 0),
             ),
             BlockType::J => BlockData::new(
-                vec![],
+                vec![
+                    vec![
+                        Vec2D::new(-1, -1),
+                        Vec2D::new(-1, 0),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(1, 0),
+                    ],
+                    vec![
+                        Vec2D::new(0, -1),
+                        Vec2D::new(1, -1),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(0, 1),
+                    ],
+                    vec![
+                        Vec2D::new(-1, 0),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(1, 0),
+                        Vec2D::new(1, 1),
+                    ],
+                    vec![
+                        Vec2D::new(0, -1),
+                        Vec2D::new(0, 0),
+                        Vec2D::new(-1, 1),
+                        Vec2D::new(0, 1),
+                    ],
+                ],
                 Colour::rgb(0, 0, 255),
             ),
         }
